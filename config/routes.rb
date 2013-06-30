2a6ac2933ee016ce60bd0098aa9a0ebbcd0354bc
JEFF <justaboutjeff@gmail.com>
2013-06-30 15:25:44 -0500

    fonts, weights

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Duo::Application.routes.draw do
 
  # Users + Teams + People
    resources :users do
-     resources :teams
-     resources :people
+     resources :teams, :only => [:create, :destroy]
+     resources :people, :only => [:create, :destroy]
    end
 
  # Sessions
-   resources :sessions, :only => :create
-   match '/logout', :to => 'sessions#destroy'
-   match '/login', :to => 'sessions#new'
+   get '/sessions/new', to: 'sessions#new', as: :login
+   post '/sessions', to: 'sessions#create'
+   delete '/sessions/:id', to: 'sessions#destroy', as: :logout
+   # resources :sessions, :only => [:create, :destroy]
+   # match '/logout', :to => 'sessions#destroy'
+   # match '/login', :to => 'sessions#new'
 end
